c2deaf5f38d987390a78d073c1d866e2bbd46ae8
2382028 <2382028@unai.edu>
2025-02-19 10:47:30 +0700

    commit baru

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom"
+import RootLayout from "./layouts/RootLayout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+import Jobs from "./pages/Jobs";
+import ContactLayout from "./layouts/ContactLayout";
+import ContactInfo from "./components/contactInfo";
+import ContactForm from "./components/contactFrom";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home/>}/>
+      <Route path ="about" element={<About/>}/>
+      <Route path ="product" element={<Product/>}/>
+      <Route path ="contact" element={<ContactLayout/>}>
+        <Route path="info" element={<ContactInfo/>}/>
+        <Route path="form" element={<ContactForm/>}/>
+        </Route>
+      <Route path ="jobs" element={<Jobs/>}/>
+        </Route>
+  )
+)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   <RouterProvider router={router}/>
+   </>
   )
 }
 
---
+++ src/components/contactInfo.tsx
@@ -0,0 +1,13 @@
+
+
+const contactInfo = () => {
+    return (
+      <div>
+          <p>Email: example@gmail.com</p>
+          <p>Phone: +6285373429842</p>
+          <p>Address: xyz street, Indonesia</p>
+      </div>
+    )
+  }
+  
+  export default contactInfo
